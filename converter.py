477b809805b07d03627cc603e75b6b8ee6de7b0d
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2019-02-25 00:41:16 +0100

    Audios should play again on legacy website

--- converter.py
+++ converter.py
@@ -8,8 +8,9 @@ def convert(input_file: str, duration: float):
 		img = Image.new("RGB", (240, 240))
 		img.save("media/img.png")
 	ff = ffmpy.FFmpeg(
-		inputs={'media/img.png': "-framerate 1/" + str(duration), input_file + '.ogg': None},
-		outputs={input_file + '.mp4': '-c:v libx264 -c:a aac -pix_fmt yuv420p'},
+		inputs={'media/img.png': None, input_file + '.ogg': None},
+		outputs={input_file + '.mp4': '-c:v libx264 -c:a aac -tune stillimage -pix_fmt yuv420p -vf tpad=stop_mode=clone:stop_duration=' + str(duration)},
 		global_options="-loglevel 0"
 	)
 	ff.run()
+#"-framerate 1/" + str(duration)
