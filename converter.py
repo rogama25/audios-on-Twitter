5a931c2f2944dc2a30204f103a48aa6172879c63
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2019-02-26 18:01:27 +0100

    Added speaker background to videos

--- converter.py
+++ converter.py
@@ -1,16 +1,14 @@
-from PIL import Image
 import os
 import ffmpy
 
 
 def convert(input_file: str, duration: float):
 	if not os.path.exists("media/img.png"):
-		img = Image.new("RGB", (240, 240))
-		img.save("media/img.png")
+		import urllib.request
+		urllib.request.urlretrieve("https://raw.githubusercontent.com/rogama25/audios-on-Twitter/master/media/img.png", "media/img.png")
 	ff = ffmpy.FFmpeg(
 		inputs={'media/img.png': None, input_file + '.ogg': None},
 		outputs={input_file + '.mp4': '-c:v libx264 -c:a aac -tune stillimage -pix_fmt yuv420p -vf tpad=stop_mode=clone:stop_duration=' + str(duration)},
 		global_options="-loglevel 0"
 	)
 	ff.run()
-#"-framerate 1/" + str(duration)
