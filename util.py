7c5cedd7b38f2737aaa599ce8d9d7d1aa208e40d
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2019-11-08 13:42:58 +0100

    Now downloading ffmpeg from official website

--- util.py
+++ util.py
@@ -23,13 +23,36 @@ def download_ffmpeg():
 	
 	:return: None
 	"""
-	if not os.path.exists("ffmpeg.exe"):
-		if not os.path.exists("ffmpeg.zip"):
-			import urllib.request
-			import zipfile
+	if not os.path.exists("ffmpeg.exe"):  # Check if ffmpeg was already downloaded and extracted
+		import zipfile
+		import urllib.request
+		version = urllib.request.urlopen("https://raw.githubusercontent.com/rogama25/audiosToTwitter/master/latest-ffmpeg.txt").read() # Get the last ffmpeg version from my repo
+		version = version.decode("UTF-8")
+		zip_valid = False
+		if os.path.exists("ffmpeg.zip"):  # Check if ffmpeg was downloaded but not extracted
+			try:
+				zip = zipfile.ZipFile("ffmpeg.zip")
+				zip_valid = True
+			except zipfile.BadZipFile:  # Corrupt zip file
+				pass
+		if not zip_valid:
+			import requests
+			import progress.bar
+			import shutil
 			print("Downloading FFMPEG. Please wait...")
-			urllib.request.urlretrieve("https://raw.githubusercontent.com/rogama25/audios-on-Twitter/master/ffmpeg.zip", "ffmpeg.zip")
-		zip = zipfile.ZipFile("ffmpeg.zip")
-		zip.extractall()
+			url = "http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-" + version + "-win32-static.zip"
+			with requests.get(url, stream=True) as r:
+				r.raise_for_status()
+				with open("ffmpeg.zip", 'wb') as f:
+					file_size = int(r.headers["Content-Length"])
+					with progress.bar.Bar("Downloading... " + str(round(file_size/1000000,2)) + " MB", suffix="%(percent)d%% ETA: %(eta)ds") as progress_bar:
+						for chunk in r.iter_content(chunk_size=8192):
+							progress_bar.next(1/(float(file_size)/8192)*100)
+							if chunk:
+								f.write(chunk)
+		zip = zipfile.ZipFile("ffmpeg.zip")  # Extract zip
+		zip.extract("ffmpeg-"+version+"-win32-static/bin/ffmpeg.exe")
+		os.rename("ffmpeg-"+version+"-win32-static/bin/ffmpeg.exe", "ffmpeg.exe")
+		shutil.rmtree("ffmpeg-"+version+"-win32-static")
 		zip.close()
 		os.remove("ffmpeg.zip")
