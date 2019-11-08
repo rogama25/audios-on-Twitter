14bd808f2d130d55fc9c6910c94e4a8d30b48a7c
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2019-11-08 16:35:56 +0100

    Added confirmation before download. Information message if ffmpeg.zip is corrupt

--- util.py
+++ util.py
@@ -34,18 +34,26 @@ def download_ffmpeg():
 				zip = zipfile.ZipFile("ffmpeg.zip")
 				zip_valid = True
 			except zipfile.BadZipFile:  # Corrupt zip file
-				pass
+				print("ffmpeg zip file detected but it's corrupt. We'll have to download it again.")
 		if not zip_valid:
 			import requests
 			import progress.bar
 			import shutil
+			import sys
+			char = input("Do you want to download ffmpeg now? This will download around 60MiB of data. [y/n] ")
+			while True:
+				if char.lower() == "n":
+					sys.exit(1)
+				if char.lower() == "y":
+					break
+				char = input("[y/n] ")
 			print("Downloading FFMPEG. Please wait...")
 			url = "http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-" + version + "-win32-static.zip"
 			with requests.get(url, stream=True) as r:
 				r.raise_for_status()
 				with open("ffmpeg.zip", 'wb') as f:
 					file_size = int(r.headers["Content-Length"])
-					with progress.bar.Bar("Downloading... " + str(round(file_size/1000000,2)) + " MB", suffix="%(percent)d%% ETA: %(eta)ds") as progress_bar:
+					with progress.bar.Bar("Downloading... " + str(round(file_size/1024/1024,2)) + " MiB", suffix="%(percent)d%% ETA: %(eta)ds") as progress_bar:
 						for chunk in r.iter_content(chunk_size=8192):
 							progress_bar.next(1/(float(file_size)/8192)*100)
 							if chunk:
