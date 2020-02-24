77ec2268da5fcf2747606805e5e186fed7096b5d
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2020-02-24 13:38:27 +0100

    Added languages loader and getter

--- languages.py
+++ languages.py
@@ -2,6 +2,9 @@ import os
 from util import get_lang_dir
 import json
 
+selected = "en"
+loaded_langs = {}
+
 
 def get_available():
     langs = {}
@@ -15,7 +18,38 @@ def get_available():
     return langs
 
 
-class Languages:
+def get_loaded():
+    global loaded_langs
+    l = []
+    for e in loaded_langs:
+        l.append(e)
+    return l
 
-    def __init__(self):
-        self.selected = None
+
+def get_string(string, lang = None):
+    if lang is None:
+        global selected
+        lang = selected
+    if lang in get_loaded():
+        global loaded_langs
+        return loaded_langs[lang].strings[string]
+    if lang in get_available():
+        l = Language(lang)
+        return l.strings[string]
+    else:
+        return get_string(string, "en")
+
+
+class Language:
+
+    def __init__(self, name):
+        if name in get_loaded():
+            raise NameError("Language already loaded.")
+        if name not in get_available():
+            raise NameError("Language not available.")
+        self.name = name
+        filename = os.path.join(get_lang_dir(), name + ".json")
+        with open(filename, "r") as file:
+            self.strings = json.load(file)
+        global loaded_langs
+        loaded_langs[name] = self
