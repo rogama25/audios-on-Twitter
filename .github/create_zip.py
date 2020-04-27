8976593285c48fae6044cda22f3adafbb018c4fb
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2020-04-28 01:55:06 +0200

    Updated Github Actions workflow

--- .github/create_zip.py
+++ .github/create_zip.py
@@ -2,13 +2,13 @@ from zipfile import ZipFile
 import os
 import shutil
 
-os.chdir("..")
 zip = ZipFile('windows.zip', 'w')
 
 for folderName, subfolders, filenames in os.walk("lang"):
-           for filename in filenames:
-               if filter(filename):
-                   filePath = os.path.join(folderName, filename)
-                   zip.write(filePath)
+    for filename in filenames:
+        filePath = os.path.join(folderName, filename)
+        zip.write(filePath)
+        print("Adding " + str(filePath))
 shutil.move("./dist/AudiosToTwitter.exe", "./AudiosToTwitter.exe")
+print("Adding .exe")
 zip.write("AudiosToTwitter.exe")
